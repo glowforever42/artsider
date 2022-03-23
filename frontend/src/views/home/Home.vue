dd01db3a423886987c38349171585220a352b25b
KimSoonYo <rlatnsdy125@naver.com>
2022-03-23 15:43:50 +0900

    Feat(FE) : 포스터 원통형 캐로셀 구현

--- frontend/src/views/home/Home.vue
+++ frontend/src/views/home/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="home">
-    <h1> hello world </h1>
+    <HomeCarousel />
   </div>
 </template>
 
 <script>
+import HomeCarousel from '@/views/home/components/HomeCarousel'
 
 export default {
   name: 'Home',
   components: {
+    HomeCarousel
   }
 }
 </script>
+<style>
+.home{
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ frontend/src/views/home/components/HomeCarousel.vue
@@ -0,0 +1,76 @@
+<template>
+  <main class="carousel-wrapper">
+    <section id="circle"> 
+    </section>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HomeCarousel',
+  data(){
+    return{
+      posters: ['poster1', 'poster2', 'poster3', 'poster4', 'poster5', 'poster6']
+    }
+  },
+
+  mounted(){
+    if(this.posters){
+      const circle = document.querySelector('#circle')
+      let yDeg = 0
+      this.posters.forEach((poster) => {
+        const article = document.createElement('article')
+        article.innerText = poster
+        circle.appendChild(article)
+      })
+
+      const articles = circle.childNodes
+
+      articles.forEach((article)=>{
+        const articleWidth = article.clientWidth
+        const zDistance = parseInt((articleWidth * 6) / 3.14) / 2
+        console.log('zDistance', zDistance)
+
+        article.style.transform = `rotateY(${yDeg}deg) translateZ(-${zDistance}px)`
+        yDeg += 60
+      })
+    }
+  }
+
+}
+</script>
+
+<style>
+@keyframes ani {
+  0% { transform: rotateY(0deg);}
+  100% {transform: rotateY(360deg);}
+}
+
+.carousel-wrapper{
+  position: relative;
+  width: 100%;
+  height: 70%;
+  perspective: 1300px;
+}
+.carousel-wrapper #circle{
+  position: absolute;  
+  width: 40%;
+  height: 100%;
+  left: 30%;
+  transform-style: preserve-3d;
+  animation: ani linear 30s infinite;
+}
+
+.carousel-wrapper #circle article{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background-color: gray;
+  backface-visibility: hidden;
+}
+
+
+</style>
