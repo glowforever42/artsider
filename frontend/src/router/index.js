c2b509e58391440fa98223fc3a5a6b66b2207306
glowforever42 <glowforever42@gmail.com>
2022-03-25 02:03:19 +0900

    Feat(FE) : 페이지 별 라우터 작업

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/home/Home.vue'
+import Intro from '../views/home/Intro.vue'
+// import Intro from './intro'
+import Main from './main'
+
+// 에티켓
+import Etiquette from './etiquette'
+
+import Classic from './etiquetteClassic'
+import KTraditionalMusic from './etiquetteKTraditionalMusic'
+import Musical from './etiquetteMusical'
+import Opera from './etiquetteOpera'
+import Theatre from './etiquetteTheatre'
+
+/// 에티켓 디테일
+import EtiquetteDetail from './etiquetteDetail'
+// import Clap from './'
+// import Phone from './'
+// import Talk from './'
+// import Time from './'
+// import Food from './'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Intro',
+    component: Intro
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // ...Intro,
+  ...Etiquette,
+  ...Main,
+  ...Classic,
+  ...KTraditionalMusic,
+  ...Musical,
+  ...Opera,
+  ...Theatre,
+
+  ...EtiquetteDetail,
+  // ...Clap,
+  // ...Phone,
+  // ...Talk,
+  // ...Time,
+  // ...Food,
 ]
 
 const router = new VueRouter({
