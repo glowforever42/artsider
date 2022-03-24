9546adb9173a27a04bf3d233f40d6be31dd53aec
KimSoonYo <rlatnsdy125@naver.com>
2022-03-25 00:16:01 +0900

    Chore(FE) : 로그인 버튼, 회원가입 버튼 통합

--- frontend/src/views/home/Home.vue
+++ frontend/src/views/home/Home.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="home">
     <HomeCarousel />
+
+    <v-btn
+      @click="loginDialog = true"
+    >
+      로그인 테스트 버튼
+    </v-btn>
+
+  
+    <Login 
+      :open="loginDialog"
+      @close-login="() => { loginDialog = false }"
+    />
+
   </div>
 </template>
 
 <script>
 import HomeCarousel from '@/views/home/components/HomeCarousel'
+import Login from '@/views/account/dialog/Login'
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      loginDialog : false
+    }
+  }, 
+
   components: {
-    HomeCarousel
+    HomeCarousel,
+    Login
+  },
+
+  methods: {
+
   }
 }
 </script>
