5cd547b4978cad480052ae87989a853e0d18448c
KimSoonYo <rlatnsdy125@naver.com>
2022-03-25 02:38:47 +0900

    Chore(FE) : 로그인 테스트 버튼 제거

--- frontend/src/views/home/Home.vue
+++ frontend/src/views/home/Home.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="home">
     <HomeCarousel />
-
-    <v-btn
-      @click="loginDialog = true"
-    >
-      로그인 테스트 버튼
-    </v-btn>
-
-  
-    <Login 
-      :open="loginDialog"
-      @close-login="() => { loginDialog = false }"
-    />
-
   </div>
 </template>
 
 <script>
 import HomeCarousel from '@/views/home/components/HomeCarousel'
-import Login from '@/views/account/dialog/Login'
+
 
 export default {
   name: 'Home',
@@ -30,8 +17,7 @@ export default {
   }, 
 
   components: {
-    HomeCarousel,
-    Login
+    HomeCarousel
   },
 
   methods: {
