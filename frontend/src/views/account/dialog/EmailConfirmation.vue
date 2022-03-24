83b57fb730c61e46f5826ce252f93c9270fd7b90
KimSoonYo <rlatnsdy125@naver.com>
2022-03-25 00:13:22 +0900

    Chore(FE) : 타이머 분 단위 시간 추가

--- frontend/src/views/account/dialog/EmailConfirmation.vue
+++ frontend/src/views/account/dialog/EmailConfirmation.vue
@@ -18,7 +18,7 @@
             <span
               slot="append"
             >
-              {{ timerCount }}
+              {{ minutes }} : {{ seconds }}
             </span>
             </v-text-field>
 
@@ -45,7 +45,9 @@ export default {
   data(){
     return{
       confirmationNumber : '',
-      timerCount: 30,
+      timerCount: 180,
+      minutes: 3,
+      seconds: 30,
       timer: null
     }
   },
@@ -68,11 +70,25 @@ export default {
   watch: {
     confirmDialog: function(){
       if(this.confirmDialog){
-        this.timerCount = 30
+        this.timerCount = 180
+        this.minutes = 3
+        this.seconds = 30
         this.startTimer()
       } else{
         clearInterval(this.timer)
       }
+    },
+
+    timerCount: function(){
+      if(this.seconds > 0){
+        this.seconds -= 1
+      } else if(this.minutes > 0) {
+        this.minutes -= 1
+        this.seconds = 59
+      } else{
+        this.$emit('close-dialog')
+      }
+
     }
   },
 
