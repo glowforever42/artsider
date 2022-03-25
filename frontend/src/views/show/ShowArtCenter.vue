3f9dc52a6f7316ef12857b1a2507f0839ab5ac81
한승훈 <gkstmdgns422@naver.com>
2022-03-25 10:52:14 +0900

    Feat(FE) : 공연장위치 추가

---
+++ frontend/src/views/show/ShowArtCenter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>공연장 위치</p>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+
+export default {
+  name: 'ShowArtCenter',
+  props: {
+    ArtCenterName: Object
+  },
+  data: function() {
+    return{
+      showArtCenterList: [],
+    }
+  },
+  methods: {
+    getshowArtCenter: function(ArtCenterName) {
+      axios({
+        method: 'get',
+        url: `http://127.0.0.1:8000/api/show/${ArtCenterName}/artcenter`,
+      })
+      .then(res => {
+        console.log(res)
+        this.showArtCenterList = res
+      })
+    }
+
+  },
+  created: function () {
+    this.showId = this.$route.params.showId
+    this.getShowArtCenter(this.ArtCenterName)
+  }
+}
+</script>
+
+<style>
+
+</style>
