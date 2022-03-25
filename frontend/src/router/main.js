eb5c871213fe37d8873a6c687f940b4a13bd6afd
glowforever42 <glowforever42@gmail.com>
2022-03-25 10:42:31 +0900

    Feat(FE) : 메인 페이지 구현 작업 및 라우터 생성

---
+++ frontend/src/router/main.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        path: '/main',
+        name: 'main',
+        component: () => import('@/views/Main.vue')
+    },
+    // {
+    //     path: '/test',
+    //     name: 'test',
+    //     component: () => import('@/views/test.vue')
+    // }
+]
