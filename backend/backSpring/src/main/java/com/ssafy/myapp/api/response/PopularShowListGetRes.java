f0f24b472f3757cbac83f6591f2379658646eba1
kcw0360 <kcw0360@naver.com>
2022-03-31 00:25:35 +0900

    Feat(BE): 필요 항목만 조회하게 하여 속도 개선(Entity에 맺어진 관계 조회x)

--- backend/backSpring/src/main/java/com/ssafy/myapp/api/response/PopularShowListGetRes.java
+++ backend/backSpring/src/main/java/com/ssafy/myapp/api/response/PopularShowListGetRes.java
@@ -1,6 +1,7 @@
 package com.ssafy.myapp.api.response;
 
 import com.ssafy.myapp.db.entity.*;
+import com.ssafy.myapp.db.mapping.ShowListMapping;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,16 +13,20 @@ import java.util.List;
 @NoArgsConstructor
 public class PopularShowListGetRes {
 
-    private Long id;
+    private Long id;    // 주의: 공연(show)의 db 에서의 pk 값, PopularShow 테이블의 pk 값이 아니다!!!
     private Integer rank;
     private String showId;
     private String sellRate;
-    private Show show;    // 공연정보
+    private String showName;    // 공연이름
+    private String startDate;    // 공연시작일
+    private String endDate;    // 공연 종료일
+    private String posterPath;    // 포스터 경로
+    private String category;    // 공연 카테고리
+    private String artCenterName;    // 공연장
 
 
 
     public PopularShowListGetRes(PopularShow entity) {
-        this.id = entity.getId();
         this.rank = entity.getRank();
         this.sellRate = entity.getSellRate();
         this.showId = entity.getShowId();
