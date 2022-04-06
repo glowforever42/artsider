c70a94a788b168e0db61f0a7582f6de925e1183d
김정윤 <kimjy199@naver.com>
2022-04-07 02:03:55 +0900

    Feat(DA): fastApi를 활용한 추천 서비스 기능

--- backend/fastApi/app/main.py
+++ backend/fastApi/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, responses
 
-from app.routers import recommendations_data_analysys
+from app.routers import recommendations_data_analysys, user_based_CF, related_show, keyword_result, tag_based_rec_show
 
 app = FastAPI()
 
@@ -9,3 +9,7 @@ def main():
     return responses.RedirectResponse(url="/docs/")
 
 app.include_router(recommendations_data_analysys.router)
+app.include_router(user_based_CF.router)
+app.include_router(related_show.router)
+app.include_router(keyword_result.router)
+app.include_router(tag_based_rec_show.router)
