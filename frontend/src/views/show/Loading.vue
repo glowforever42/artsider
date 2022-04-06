f17fb125d8826382352bdc3b884388bf8263d407
한승훈 <gkstmdgns422@naver.com>
2022-04-06 17:15:16 +0900

    Feat(FE) : 로고, 로딩스피너 추가

---
+++ frontend/src/views/show/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex justify-center align-center" style="height:80vh; width:100%">
+    <v-progress-circular
+      :size="80"
+      :width="8"
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+}
+</script>
+
+<style>
+
+</style>
