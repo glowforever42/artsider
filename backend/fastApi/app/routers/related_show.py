d9e89024d10a8712c7bb8d37c89b9c69d0889092
glowforever42 <76445655+glowforever42@users.noreply.github.com>
2022-06-03 15:29:58 +0900

    DB 접속 정보 삭제
    DB 접속 정보 삭제

--- backend/fastApi/app/routers/related_show.py
+++ backend/fastApi/app/routers/related_show.py
@@ -12,10 +12,10 @@ router = APIRouter()
 
 @router.get("/recommand/relatedShow", status_code=200)
 def related_show():
-    password = 'artsider202!'
-    pwd = parse.quote_plus(password)
+    password = 
+    pwd = 
 
-    db_connection_str = 'mysql+pymysql://artsider:'+pwd+'@j6b202.p.ssafy.io/artsider'
+    db_connection_str = 
     db_connection_engine = create_engine(db_connection_str, encoding='utf-8')
 
     def cos_sim(A, B):
